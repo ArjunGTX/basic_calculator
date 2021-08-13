0047c57889da5e9d173aa126de582d880864f7b6
ArjunGTX <arjunatpavilion@gmail.com>
2021-08-13 23:19:26 +0530

    working version finished

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-
-function readText(ownerObject) {
-    valueInsideElement = ownerObject.innerText;
-    
-   
+function readNumber(ownerObject) {
+  let valueInsideElement = ownerObject.innerText;
+  displayOnScreen(valueInsideElement);
+}
+function readOperator(ownerObject) {
+  let valueInsideElement = ownerObject.innerText;
+  let valueOnScreen = document.querySelector(".screen");
+  if (valueOnScreen.innerText == "") {
+    return;
+  } else {
+    displayOnScreen(valueInsideElement);
+  }
 }
 
-
+function displayOnScreen(valueInsideElement) {
+  let valueOnScreen = document.querySelector(".screen");
+  valueOnScreen.innerText += valueInsideElement;
+  document.getElementsByClassName("screen").innerText = valueOnScreen;
+}
+function readAllClear() {
+  let valueOnScreen = document.querySelector(".screen");
+  valueOnScreen.innerText = "";
+}
+function readClear() {
+  let valueOnScreen = document.querySelector(".screen");
+  let number = valueOnScreen.innerText.slice(0, -1);
+  valueOnScreen.innerText = number;
+}
+function readEquals() {
+  let valueOnScreen = document.querySelector(".screen");
+  let number = valueOnScreen.innerText;
+  readAllClear();
+  let result = eval(number);
+  displayOnScreen(result);
+}
